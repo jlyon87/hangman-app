9c50518d9830983ef4e9171f54b3f8450b801443
jlyonas <jlyon@acumensolutions.com>
2017-07-31 08:50:12 -0400

    init hm stats and word

--- assets/js/hm-stats.js
+++ assets/js/hm-stats.js
@@ -1,3 +1,23 @@
 var HangmanStats = (function HangmanStats() {
+	var wins = 0;
+	var remainingGuesses;
 
+	return {
+		wins: wins,
+		remainingGuesses: remainingGuesses,
+		hasRemainingGuesses: hasRemainingGuesses,
+		setRemainingGuesses: setRemainingGuesses,
+	};
+
+	function hasRemainingGuesses() {
+		return (remainingGuesses != 0);
+	}
+
+	function setRemainingGuesses(wordLength) {
+		if(wordLength > 6) {
+			remainingGuesses = 12
+		} else {
+			remainingGuesses = wordLength * 2;
+		}
+	}
 }());
--- assets/js/hm-word.js
+++ assets/js/hm-word.js
@@ -1,3 +1,27 @@
 var HangmanWord = (function HangmanWord() {
+	var word;
+	var scrubbedWord;
+	var lettersGuessed;
 
+	return {
+		word: word,
+		scrubbedWord: scrubbedWord,
+		lettersGuessed: lettersGuessed,
+		hasGuessedLetter: hasGuessedLetter,
+		hasWon: hasWon,
+		updateScrubbedWord: updateScrubbedWord,
+		initScrubbedWord: initScrubbedWord,
+	};
+
+	function hasGuessedLetter(letter) {
+		return lettersGuessed.includes(letter);
+	}
+
+	function hasWon() {
+		return !scrubbedWord.includes("_");
+	}
+
+	function updateScrubbedWord() {}
+
+	function initScrubbedWord() {}
 }());
